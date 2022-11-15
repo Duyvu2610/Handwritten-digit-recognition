5c161edd51cc4b9f4e3adbcd6175c636619b4d40
Your Name <you@example.com>
2022-11-15 22:21:54 +0700

    v2

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-
+import matplotlib.pyplot as plt
 from tensorflow import keras
 import cv2
 import numpy as np
 
-model = keras.models.load_model('D:\Work space\\ai\mnist.hdf5')
-st.title("MNIST Digit Recognizer")
+model = keras.models.load_model('D:\Work space\\ai\mnist2.hdf5')
+st.title("MNIST Digit Recognizer: DuyVudz")
 
 
 # Create a canvas component
@@ -20,22 +20,20 @@ canvas_result = st_canvas(
     width=150,
     key="canvas",
 )
-col1, col2 = st.columns(2)
 
-SIZE = 192
+SIZE = 196
 # Do something interesting with the image data and paths
 if canvas_result.image_data is not None:
-    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+    img = cv2.resize(canvas_result.image_data, (28, 28))
     img_rescaling = cv2.resize(
         img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-    with col1:
-        st.write('Input Image')
+
 
 predict = st.button('Predict')
 
 if predict:
     test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    st.image(test_x)
     pred = model.predict(test_x.reshape(1, 28, 28, 1))
-    with col2:
-        st.write(f'result: {np.argmax(pred[0])}')
-        st.bar_chart(pred[0])
+    st.write(f'result: {np.argmax(pred[0])}')
+    st.bar_chart(pred[0])
